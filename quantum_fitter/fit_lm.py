fd1ff041033044641eb2bdcc2fb081d98c723166
Gao Zheming <gao_zm87@yahoo.com>
2021-04-06 23:46:31 +0200

    try to update

---
+++ quantum_fitter/fit_lm.py
@@ -0,0 +1,13 @@
+from lmfit.models import LorentzianModel, GaussianModel, ExponentialModel
+import numpy as np
+
+class FitFromLMFit:
+    def __init__(self, data_x, data_y, model=None):
+        self.model = model
+        self.data_x = data_x
+        self.data_y = data_y
+
+    def do_fit
+
+    def fit_through_lm
+
